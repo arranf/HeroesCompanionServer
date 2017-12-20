ec834a0c0632abc645e782c3a3f71864227b1205
Arran France <arranf@gmail.com>
2017-12-20 01:26:28 +0000

    Initial Version

---
+++ fetch.js
@@ -0,0 +1,65 @@
+
+const Nightmare = require('nightmare');
+const jquery = require('jquery'); 
+const nightmare = new Nightmare({ show: false });
+const chrono = require('chrono-node')
+const fs = require('fs');
+
+function fetch() {
+  const regex = /(\d{1,2}-[A-Za-z]{3,4}-\d{4}) [–,-] (\d{1,2}-[A-Za-z]{3,4}-\d{4})/;
+  console.log('Fetching data')
+  // Nightmare example: https://github.com/nelsonkhan/nightmare-gigs/blob/master/gigs.js
+  nightmare
+      .goto('http://eu.battle.net/heroes/en/heroes/')
+      .wait('.hero-list:last-child')
+      .evaluate(() => {
+        let heroes = [];
+        $('.hero-container:not(.placeholder)')
+        .each((i, el) => {
+          el = $(el)
+          item = {}
+          var name = el.find('.hero-list__item__name')[0]
+          if (name){
+            item.name = name.innerText
+          }
+
+          var title = el.find('.hero-list__item__title')[0]
+          if (title) {
+            item.title = title.innerText
+          }
+
+          var isFreeToPlay = $(el).find('.hero-list__item__free-flag')
+          item.isFreeToPlay = isFreeToPlay.length > 0
+
+          heroes.push(item)
+        })
+        let durationNodes = $('.free-rotation__date')
+        if (!durationNodes || durationNodes.length <= 0){ 
+          throw new Error('Things broke: Cant determine duration')
+        }
+        let duration = durationNodes[0].innerText
+        
+        return {'heroes': heroes, 'duration': duration}
+      })
+      .end()
+      .then((data) => {
+        let duration = data['duration']
+        let matches = regex.exec(data['duration'])
+        if (!matches || matches.length != 3){
+          throw new Error('Duration string matching failed')
+        }
+        let start = chrono.parseDate(matches[1])
+        let end = chrono.parseDate(matches[2])
+        data['start'] = start
+        data['end'] = end
+        fs.writeFile('data.json', JSON.stringify(data), (err) => {  
+          // throws an error, you could also catch it here
+          if (err) throw err;
+      
+          // success case, the file was saved
+          console.log(`Data Saved ${new Date()}`);
+      })
+    })
+}
+
+module.exports = fetch
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+var cron = require('node-cron');
+
+var fetch = require('./fetch');
+ 
+cron.schedule('15,45 * * * *', function(){
+  console.log('Cron Running')
+  fetch();
+}, true);
+
+
+function readData() {
+  fs.readFile('./data.json', 'utf8', function (err,d) {
+    if (err) {
+      return console.log(err);
+    }
+    data = d
+  });
+}
+
+let data = ''
+readData()
+let lastRead = Date.now()
+
+app.get('/', function (req, res) {
+  //60000
+  if (Date.now()  > lastRead + (6 * 15)) {
+    readData()
+    lastRead =  Date.now()
+  }
+  res.send(data);
+})
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
