60841d0eaa0165942aa039b82619ea569e9ce36c
Arran France <arranf@gmail.com>
2018-02-05 01:09:48 +0000

    Remove redundant dependency and add shrink-ray for caching

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+let shrinkRay = require('shrink-ray')
 let rotationData = require('./services/rotation_service');
 let patchData = require('./services/patch_service');
 let {updateData, updateId} = require('./services/update_service');
 let tipData = require('./services/tips_service')
-const enforce = require('express-sslify');
 
-// app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(shrinkRay())
 
 // Serve data at root domain
 app.get('/', function (req, res) {
