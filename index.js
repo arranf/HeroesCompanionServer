401b6dbd620f963682ff7f4fc8260be7bc041ab6
Arran France <arranf@gmail.com>
2018-02-18 18:53:33 +0000

    Add endpoint to add builds

--- index.js
+++ index.js
@@ -16,9 +16,16 @@ let {
 // DB
 const mongoose = require('mongoose');
 const Build = require('./models/build');
+const Hero = require('./models/hero');
+const Talent = require('./models/talent');
+
+// Md5 hashing builds
+const crypto = require('crypto');
 
 // Compression
 app.use(shrinkRay());
+// Body parsing
+app.use(express.json());
 
 // Routes
 app.get('/', function (req, res) {
@@ -63,6 +70,57 @@ app.get('/v1/builds/:hero', function (req, res) {
     }
     res.json(builds);
   });
+});
+
+app.post('/v1/builds', async function (req, res) {
+  let newBuild = req.body;
+  if (!((newBuild.HeroName || newBuild.HeroId) && newBuild.Talents.length === 7)) {  
+    res.status(500).send('Requires: HeroName/HeroId and 7 Talents');
+    return;
+  }
+
+  let hero;
+  if (newBuild.HeroName) {
+    hero = await Hero.findOne({ 'Name': newBuild.HeroName }).exec(); 
+  } else {
+    hero = await Hero.findOne({'HeroId': newBuild.HeroId}).exec();
+  }
+
+  let talentQueries = [];
+  newBuild.Talents.forEach(talent => {
+    if (!(talent.Name || talent.TalentTreeId)) {
+      res.status(500).send('Invalid talent');
+      return;
+    }
+    if (talent.Name) {
+      talentQueries.push(Talent.findOne({'Name': talent.Name, 'HeroId': hero.HeroId}).exec());
+    } else {
+      talentQueries.push(Talent.findOne({'TalentTreeId': talent.TalentTreeId, 'HeroId': hero.HeroId}).exec());
+    }
+  });
+
+  Promise.all(talentQueries)
+    .then(results => {
+      if (results.includes(null)) {
+        res.status(500).send('Invalid talent');
+        return;
+      }
+
+      let talents = results.map(r => ({Name: r.Name, TalentTreeId: r.TalentTreeId}));
+      // To make hash deterministic
+      talents.sort((a, b) => {
+        if (a.Name < b.Name) return -1;
+        if (a.Name > b.Name) return 1;
+        return 0;
+      });
+      const md5 = crypto.createHash('md5').update(`${hero.HeroId}${talents.map(t => t.Name).join('')}`).digest('hex');
+      let build = new Build({ HeroId: hero.HeroId, Description: newBuild.Description, Talents: talents, Url: newBuild.Url, Md5: md5 });
+      return build.save();
+  })
+  .then(data => {
+    res.status(200).send();
+  })
+  .catch((e) => {console.error(e); res.status(500).send('Error saving build. Likely duplicate.');});
 });
 
 // Connect to DB
