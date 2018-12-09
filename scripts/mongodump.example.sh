bcfde72b39a632cab8bc79744cadfe492b86ac23
Arran France <arranf@gmail.com>
2018-12-09 13:49:36 +0000

    Add DB restore/dump scripts

---
+++ scripts/mongodump.example.sh
@@ -0,0 +1,3 @@
+# Export from production
+# Replace <user> and <password>
+mongodump -h ds241548-a0.mlab.com:41548 -d heroescompanion -u <user> -p <password> -o .
---
+++ scripts/mongorestore.sh
@@ -0,0 +1,2 @@
+# Restores production data to a passwordless localhost
+mongorestore -h localhost:27017 -d heroescompanion heroescompanion/
